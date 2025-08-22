e4fd50ec8bacc2167b006a54c10b3359b6fa4c50
DESKTOP-1P9MEJT\rober <robertolopez612@gmail.com>
2025-08-23 00:59:10 +0200

    Login/welcome component was modified (template and styles) to make it more attractive.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,18 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {CommonModule} from '@angular/common';
 import {Router} from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule],
   templateUrl: './login.component.html',
   standalone: true,
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
-  constructor(private auth: AuthService, private router: Router) {}
+  constructor(private auth: AuthService, private router: Router) {
+  }
 
   login() {
     this.auth.login();
